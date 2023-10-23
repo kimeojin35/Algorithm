bb3b61b2a4aebb1b71ad42977982dec16c9f62f0
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-10-23 13:52:15 +0900

    [level unrated] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,12 +1,3 @@
 function solution(before, after) {
-    var answer = 0;
-    before = before.split("").sort();
-    after = after.split("").sort();
-    
-    for (let i = 0; i < before.length; i++) {
-        if (before[i] != after[i]) {
-            return 0;
-        }
-    }
-    return 1;
+    return before.split("").sort().join("") == after.split("").sort().join("") ? 1 : 0;
 }
