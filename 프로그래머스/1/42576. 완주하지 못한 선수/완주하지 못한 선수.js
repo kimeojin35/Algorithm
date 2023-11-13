44a8515d59920e28adebda36aa7126e756f27d51
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-11-13 17:31:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.29 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,28 +1,8 @@
 function solution(participant, completion) {
-    const participantMap = new Map();
-
-    // 참가자 목록을 Map으로 변환
-    participant.forEach(name => {
-        if (participantMap.has(name)) {
-            participantMap.set(name, participantMap.get(name) + 1);
-        } else {
-            participantMap.set(name, 1);
-        }
-    });
-
-    // 완주자 목록을 참가자 Map에서 빼기
-    completion.forEach(name => {
-        if (participantMap.has(name)) {
-            const count = participantMap.get(name);
-            if (count === 1) {
-                participantMap.delete(name);
-            } else {
-                participantMap.set(name, count - 1);
-            }
-        }
-    });
-
-    // Map에 남아있는 참가자가 답
-    const result = Array.from(participantMap.keys())[0];
+    participant.sort();
+    completion.sort();
+    for (let i = 0; i < participant.length; i++) {
+        if (participant[i] != completion[i]) return participant[i];
+    }
     return result;
 }
