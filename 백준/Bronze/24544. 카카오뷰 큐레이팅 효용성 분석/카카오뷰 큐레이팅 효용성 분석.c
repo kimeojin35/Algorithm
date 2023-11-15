f072b81770c8768c82914e57a603f62a620b3be6
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-11-15 19:26:11 +0900

    [Bronze II] Title: 카카오뷰 큐레이팅 효용성 분석, Time: 0 ms, Memory: 1772 KB -BaekjoonHub

---
+++ 백준/Bronze/24544. 카카오뷰 큐레이팅 효용성 분석/카카오뷰 큐레이팅 효용성 분석.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int n, m;
+	int h1 = 0, h2 = 0;
+	int a1[100000];
+	int a2[100000];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &m);
+		a1[i] = m;
+		h1 += m;
+	}
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &m);
+		a2[i] = m;
+	}
+	for (int i = 0; i < n; i++) {
+		if (a2[i] != 1) h2 += a1[i];
+	}
+	printf("%d\n", h1);
+	printf("%d\n", h2);
+	return 0;
+}
