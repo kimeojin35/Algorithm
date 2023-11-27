0ab0367057e0cd76b28df9d190a114fee4d71aaa
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-11-27 19:56:36 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = [n];
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        answer.push(n);
+    }
+    return answer;
+}
