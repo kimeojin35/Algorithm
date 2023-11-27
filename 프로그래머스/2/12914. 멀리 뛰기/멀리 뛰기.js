8f24c7f1b564d60fe589c408084ec5c074b8686a
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-11-27 19:48:52 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    return dp[n];
+}
