0323331355208088a42d816a10259aa21c7049ce
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2023-11-27 20:28:16 +0900

    [level 3] Title: 정수 삼각형, Time: 3.96 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,11 @@
+function solution(triangle) {
+    const height = triangle.length;
+
+    for (let i = height - 2; i >= 0; i--) {
+        for (let j = 0; j <= i; j++) {
+            triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+        }
+    }
+
+    return triangle[0][0];
+}
