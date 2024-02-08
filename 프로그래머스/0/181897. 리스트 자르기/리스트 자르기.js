b04b57ee3f5853baab866a1ef4c707db628fe684
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-02-08 10:43:59 +0900

    [level 0] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,13 +1,10 @@
 function solution(n, slicer, num_list) {
     var answer = [];
     let [a, b, c] = slicer;
-    if (n == 1) {
-        return num_list.slice(0, b+1)
-    } else if (n == 2) {
-        return num_list.slice(a)
-    } else if (n == 3) {
-        return num_list.slice(a, b+1)
-    } else {
+    if (n == 1) return num_list.slice(0, b+1)
+    if (n == 2) return num_list.slice(a)
+    if (n == 3) return num_list.slice(a, b+1)
+    if (n == 4) {
         let ans = num_list.slice(a, b+1)
         for (let i = 0; i < ans.length; i+=c) {
             answer.push(ans[i]);
