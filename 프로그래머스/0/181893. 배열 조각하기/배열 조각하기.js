88c46b56c83110d466e9c4006534e084f00f02ed
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-02-13 14:05:17 +0900

    [level 0] Title: 배열 조각하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -1,7 +1,4 @@
 function solution(arr, query) {
-    query.map((i, j) => {
-        console.log(arr);
-        j % 2 == 0 ? arr.splice(i + 1, arr.length) : arr.splice(0, i);
-    console.log(arr)});
+    query.map((i, j) => j % 2 == 0 ? arr.splice(i + 1, arr.length) : arr.splice(0, i));
     return arr;
 }
