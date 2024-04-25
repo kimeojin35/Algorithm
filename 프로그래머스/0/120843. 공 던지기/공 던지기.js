16dde56e4b9d8cddc1332b273da0ad6e15194441
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-04-25 23:46:57 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -1,3 +1,10 @@
 function solution(numbers, k) {
-  return numbers[((k - 1) * 2) % numbers.length];
+    var answer = 0;
+    let o = 0;
+    for (let i = 0; i < k; i++) {
+        for (let j = 0; i < 2; j++) {
+            if (o == numbers[numbers.length - 1])
+        }
+    }
+    return answer;
 }
