32a7b35bfe94874ec788c03f033a12cedae49793
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-06-26 23:45:34 +0900

    [level 2] Title: n^2 배열 자르기, Time: 6.35 ms, Memory: 43.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -0,0 +1,6 @@
+function solution(n, left, right) {
+    var answer = [];
+    for(var i=left; i<=right; i++)
+        answer.push(Math.max(Number.parseInt(i / n), i % n) + 1);
+    return answer;
+}
