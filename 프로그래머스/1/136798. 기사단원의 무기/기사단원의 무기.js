692920d641279b808760bf713f2f6a69e320897e
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-06-29 23:59:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.59 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,23 @@
+function solution(number, limit, power) {
+  let res = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let cnt = 0;
+
+    // 약수 개수 구하기
+    for (let j = 1; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        if (i / j === j) cnt += 1;
+        else cnt += 2;
+      }
+      if (cnt > limit) {
+        cnt = power;
+        break;
+      }
+    }
+
+    res += cnt;
+  }
+
+  return res;
+}
