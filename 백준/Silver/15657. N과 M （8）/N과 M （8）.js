2b02b13367ac7a37c5443d755e92101536fb0a83
김어진 <129836778+kimeojin35@users.noreply.github.com>
2025-03-04 19:13:31 +0900

    [Silver III] Title: N과 M (8), Time: 220 ms, Memory: 22348 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const [num, testcase] = fs.readFileSync(0).toString().trim().split("\n");
+const [n, m] = num.split(" ").map(Number);
+const arr = testcase
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const getCombinations = function (arr, selectNumber) {
+  const results = [];
+  if (selectNumber === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+    const rest = origin.slice(index);
+    const combinations = getCombinations(rest, selectNumber - 1);
+    const attached = combinations.map((el) => [fixed, ...el]);
+    results.push(...attached);
+  });
+
+  return results;
+};
+
+console.log(
+  getCombinations(arr, m)
+    .map((i) => i.join(" "))
+    .join("\n")
+);
