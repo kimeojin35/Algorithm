971427f3cd7764036e2a60c89562348f511d8cd8
김어진 <129836778+kimeojin35@users.noreply.github.com>
2025-06-17 17:29:57 +0900

    [Gold V] Title: 토마토, Time: 732 ms, Memory: 167000 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,59 +1,42 @@
 const fs = require('fs')
 const [[m, n], ...arr] = fs.readFileSync(0).toString().trim().split("\n").map((i) => i.split(" ").map(Number));
-const move = [
-  [1, 0],
-  [0, 1],
-  [-1, 0],
-  [0, -1],
-];
+const move = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
 const visited = Array.from({ length: n }, () => Array(m).fill(0));
 let remain = n * m;
-
 const queue = [];
 let head = 0;
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    if (arr[i][j] === 1) {
+arr.map((line, i) => {
+  line.map((item, j) => {
+    if (item == 1) {
       queue.push([i, j]);
       visited[i][j] = 1;
       remain--;
-    } else if (arr[i][j] === -1) {
+    } else if (item == -1) {
       visited[i][j] = 1;
       remain--;
     }
-  }
-}
+  });
+});
 
 let count = 0;
 while (head < queue.length) {
-  if (remain === 0) break;
-
+  if (remain == 0) break;
   const len = queue.length - head;
   for (let i = 0; i < len; i++) {
-    const [x, y] = queue[head++];
-
-    for (const [dx, dy] of move) {
-      const nx = x + dx;
-      const ny = y + dy;
-
-      if (
-        nx >= 0 &&
-        nx < n &&
-        ny >= 0 &&
-        ny < m &&
-        arr[nx][ny] === 0 &&
-        visited[nx][ny] === 0
-      ) {
-        visited[nx][ny] = 1;
-        queue.push([nx, ny]);
+    const [nx, ny] = queue[head++];
+    move.map(([x, y]) => {
+      const dx = nx + x;
+      const dy = ny + y;
+      if (dx >= 0 && dx < n && dy >= 0 && dy < m && arr[dx][dy] == 0 && visited[dx][dy] == 0) {
+        visited[dx][dy] = 1;
+        queue.push([dx, dy]);
         remain--;
       }
-    }
+    });
   }
   count++;
 }
-
-if (remain > 0) count = -1;
+if (remain) count = -1;
 console.log(count);
